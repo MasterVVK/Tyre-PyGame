b000fab50607da849e4ba3e4106e65b927fdb1be
MasterVVK <vvk@2140101.ru>
2024-03-24 17:56:14 +0500

    Добавлена работа в цикле

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import time
 
 pygame.init()
 
@@ -13,14 +14,35 @@ pygame.display.set_icon(icon)
 target_img = pygame.image.load("PICs/target.png")
 target_width = 50
 target_height = 50
+target_img = pygame.transform.scale(target_img, (target_width, target_height))
 target_x = random.randint(0,SCREEN_WIDTH - target_width )
 target_y = random.randint(0,SCREEN_HEIGHT - target_height)
 
 color_bg = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+#print(color_bg)
+#pygame.display.flip()
+#pygame.display.update()
 
 running = True
 while running:
-    pass
+    screen.fill(color_bg)
+    screen.blit(target_img,(target_x, target_y))
+    pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                color_bg_click = (255, 0, 0)
+                screen.fill(color_bg_click)
+                pygame.display.update()
+                time.sleep(0.1)
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+            #print(mouse_x,mouse_y)
+
 
 pygame.quit()
 

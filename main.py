630b420091d59a93204024e5b65735910230afbf
MasterVVK <vvk@2140101.ru>
2024-03-24 15:51:33 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import pygame
 
 pygame.init()
 
-running = True
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Домашний тир")
+icon = pygame.image.load("free-icon-shooting-range-2689911.png")
 
+running = True
 while running:
     pass
 

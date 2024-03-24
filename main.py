321534ac93736c957f8c18c528edf8719f8de3dd
MasterVVK <vvk@2140101.ru>
2024-03-24 16:24:56 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -6,7 +7,16 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Домашний тир")
-icon = pygame.image.load("free-icon-shooting-range-2689911.png")
+icon = pygame.image.load("PICs/free-icon-shooting-range-2689911.png")
+pygame.display.set_icon(icon)
+
+target_img = pygame.image.load("PICs/target.png")
+target_width = 50
+target_height = 50
+target_x = random.randint(0,SCREEN_WIDTH - target_width )
+target_y = random.randint(0,SCREEN_HEIGHT - target_height)
+
+color_bg = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
 
 running = True
 while running:
